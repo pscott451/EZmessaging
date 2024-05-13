143ddcf0c4ba68c7657a0a25385b20af134aef2b
Phillip Scott <phillipscott@users-MBP.home>
2024-05-13 13:55:04 -0700

    yml

--- ezmessaging/build.gradle.kts
+++ ezmessaging/build.gradle.kts
@@ -33,6 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+    publishing {
+        multipleVariants {
+            withSourcesJar()
+            withJavadocJar()
+            allVariants()
+        }
+    }
 }
 
 dependencies {
@@ -72,4 +79,14 @@ dependencies {
     api(libs.sharedtest.kotest.api)
     api(libs.sharedtest.kotest.core)
     api(libs.sharedtest.kotest.shared)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.pscott451"
+            artifactId = "EZmessaging"
+            version = "0.0.10"
+        }
+    }
 }
