ecce70feb0e9c8e0c5c1c2e957728d7f3b3f781c
Phillip Scott <phillipscott@Phillips-MBP.home>
2024-07-26 14:04:25 -0700

    update version

--- buildSrc/src/main/kotlin/Jitpack.kt
+++ buildSrc/src/main/kotlin/Jitpack.kt
@@ -2,5 +2,5 @@ object Jitpack {
     const val GROUP_ID = "com.github.pscott451"
     const val EZ_MESSAGING_ARTIFACT_ID = "ezmessaging"
     const val EZ_MESSAGING_GOOGLE_ARTIFACT_ID = "ezmessaginggoogle"
-    const val VERSION = "2.0.0"
+    const val VERSION = "2.0.1"
 }
